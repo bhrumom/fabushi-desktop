93dadaf9d4abe9e002c44aa5f978dbb8b652843f
bhrum <bhrumom@gmail.com>
2026-09-22 15:18:12 +0800

    feat(host): publish conversation parity surface

--- source/host/src/lib.rs
+++ source/host/src/lib.rs
@@ -13,13 +13,17 @@ pub mod gateway_server;
 pub mod runner;
 
 pub use runner::{
-    AttemptCheckpoint, AttemptProgress, CheckpointBoundary, HIDDEN_PROMPT_MARKER,
-    MAX_SAFE_TOKEN_COUNT, ModelResolutionTicket, RecentUserMessage, ResolvedModelTracker,
-    RetryDecision, SUMMARIZATION_MAX_PROMPT_CHARS, SanitizedUsage, StreamAttemptPolicy,
-    StreamFailureKind, StreamWatchdog, TOOL_CALL_IDENTITY_CAP, TerminalOutcome, ToolCallIdentity,
+    AttemptCheckpoint, AttemptProgress, CheckpointBoundary, CompletedAwaitOutcome,
+    ContextWindowTracker, FullStreamSanitizer, HIDDEN_PROMPT_MARKER, MAX_SAFE_TOKEN_COUNT,
+    ModelResolutionTicket, RecentUserMessage, ResolvedModelTracker, RetryDecision,
+    SUMMARIZATION_MAX_OUTPUT_TOKENS, SUMMARIZATION_MAX_PROMPT_CHARS, SanitizedExtendedUsage,
+    SanitizedUsage, StreamAttemptPolicy, StreamFailureKind, StreamSanitizerItem, StreamWatchdog,
+    SummarizationPolicy, TOOL_CALL_IDENTITY_CAP, TerminalOutcome, ToolCallIdentity,
     ToolSurfaceUpdate, TransientStreamError, TurnCancellation, TurnEndedUsage, TurnOwnerToken,
     TurnRunFinished, TurnRunOptions, TurnRunShell, TurnRunShellError, TurnRunStarted,
-    TurnSettlement, TurnUsage, add_token_counts, build_unanswered_questions_note,
-    merge_turn_usage, sanitize_usage, select_unconfirmed_user_messages, should_use_self_summary,
-    to_safe_token_count, to_safe_usage_count, total_input_tokens, turn_usage_from_turn_ended,
+    TurnSettlement, TurnUsage, add_token_counts, await_block_until_ms,
+    build_unanswered_questions_note, classify_completed_await_outcome, merge_turn_usage,
+    sanitize_usage, select_unconfirmed_user_messages, should_use_self_summary,
+    summarization_policy, to_safe_token_count, to_safe_usage_count, total_input_tokens,
+    turn_usage_from_turn_ended,
 };
